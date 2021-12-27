cd38eef2128073ee850609ee0b4d24fda599886a
alicenyberg <alice.nyberg@hotmail.com>
2021-12-27 10:59:36 +0100

    added more to register.php

--- app/users/register.php
+++ app/users/register.php
@@ -10,4 +10,14 @@ if (isset($_POST['username'], $_POST['email'], $_POST['password'])) {
     $email = filter_var($trimmed_email, FILTER_SANITIZE_EMAIL);
     $hashed_password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 }
+
+$statement = $database->prepare('SELECT email FROM users WHERE email = :email');
+$statement->bindParam(':email', $email, PDO::PARAM_STR);
+$statement->execute();
+$compareEmail = $statement->fetch(PDO::FETCH_ASSOC);
+if ($compareEmail) {
+    $_SESSION['errors'][] = "Maybe try another email, this already exists!";
+    redirect('/register.php');
+}
+
 redirect('/');
