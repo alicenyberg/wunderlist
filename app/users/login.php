0da2d4dd5d03cfe4ad030066321f9d440887709d
alicenyberg <alice.nyberg@hotmail.com>
2021-12-27 10:59:20 +0100

    added session info

--- app/users/login.php
+++ app/users/login.php
@@ -6,22 +6,20 @@ require __DIR__ . '/../autoload.php';
 
 if (isset($_POST['email'], $_POST['password'])) {
     $email = trim($_POST['email']);
-
-
     $statement = $database->prepare('SELECT * FROM users WHERE email = :email');
     $statement->bindParam(':email', $email, PDO::PARAM_STR);
     $statement->execute();
-
     $user = $statement->fetch(PDO::FETCH_ASSOC);
 
-    if (!$user) {
-        redirect('/login.php');
+    if ($user === false) {
+        redirect('/index.php');
     }
     if (password_verify($_POST['password'], $user['password'])) {
-        unset($user['password']);
-
-        $_SESSION['user'] = $user;
+        $_SESSION['user'] = [
+            "id" => $user['id'],
+            "name" => $user['username'],
+            "email" => $user['email'],
+        ];
+        redirect('/');
     }
 }
-
-redirect('/');
