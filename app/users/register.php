de6f88947f640e2d3272945112166b82e510f49d
alicenyberg <alice.nyberg@hotmail.com>
2021-12-29 14:18:49 +0100

    register a new user

--- app/users/register.php
+++ app/users/register.php
@@ -9,26 +9,37 @@ if (isset($_POST['username'], $_POST['email'], $_POST['password'])) {
     $trimmed_email = trim($_POST['email']);
     $email = filter_var($trimmed_email, FILTER_SANITIZE_EMAIL);
     $hashed_password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-}
 
-// if the email already exist in database:
-$statement = $database->prepare('SELECT email FROM users WHERE email = :email');
-$statement->bindParam(':email', $email, PDO::PARAM_STR);
-$statement->execute();
-$compareEmail = $statement->fetch(PDO::FETCH_ASSOC);
-if ($compareEmail) {
-    $_SESSION['errors'][] = "Maybe try another email, this already exists!";
-    redirect('/register.php');
-}
+    // if the email already exist in database:
+    $statement = $database->prepare('SELECT email FROM users WHERE email = :email');
+    $statement->bindParam(':email', $email, PDO::PARAM_STR);
+    $statement->execute();
+    $compareEmail = $statement->fetch(PDO::FETCH_ASSOC);
+    if ($compareEmail) {
+        $_SESSION['errors'][] = "Maybe try another email, this already exists!";
+        redirect('/register.php');
+    }
 
-//if the username already exists in database:
-$statement = $database->prepare('SELECT username FROM users WHERE username = :username');
-$statement->bindParam(':username', $username, PDO::PARAM_STR);
-$statement->execute();
-$checkUsername = $statement->fetch(PDO::FETCH_ASSOC);
-if ($checkUsername) {
-    $_SESSION['errors'][] = "Maybe try another username, this already exists!";
-    redirect('/register.php');
-}
+    //if the username already exists in database:
+    $statement = $database->prepare('SELECT username FROM users WHERE username = :username');
+    $statement->bindParam(':username', $username, PDO::PARAM_STR);
+    $statement->execute();
+    $checkUsername = $statement->fetch(PDO::FETCH_ASSOC);
+    if ($checkUsername) {
+        $_SESSION['errors'][] = "Maybe try another username, this already exists!";
+        redirect('/register.php');
+    }
+    //add a new user in database:
+    $statement = $database->prepare('INSERT INTO users
+   (username, email, password)
+   VALUES
+   (:username, :email, :password)');
 
-redirect('/');
+    $statement->bindParam(':username', $username, PDO::PARAM_STR);
+    $statement->bindParam(':email', $email, PDO::PARAM_STR);
+    $statement->bindParam(':password', $hashed_password, PDO::PARAM_STR);
+
+    $statement->execute();
+
+    redirect('/');
+}
