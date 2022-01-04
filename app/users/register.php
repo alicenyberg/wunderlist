e1476aa866618dc28e2540de229e5d6bf4fe9df5
alicenyberg <alice.nyberg@hotmail.com>
2022-01-04 13:57:54 +0100

    updated register

--- app/users/register.php
+++ app/users/register.php
@@ -6,8 +6,7 @@ require __DIR__ . '/../autoload.php';
 
 if (isset($_POST['username'], $_POST['email'], $_POST['password'])) {
     $username = trim($_POST['username']);
-    $trimmed_email = trim($_POST['email']);
-    $email = filter_var($trimmed_email, FILTER_SANITIZE_EMAIL);
+    $email = trim($_POST['email']);
     $hashed_password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
     // if the email already exist in database:
@@ -15,7 +14,7 @@ if (isset($_POST['username'], $_POST['email'], $_POST['password'])) {
     $statement->bindParam(':email', $email, PDO::PARAM_STR);
     $statement->execute();
     $compareEmail = $statement->fetch(PDO::FETCH_ASSOC);
-    if ($compareEmail) {
+    if ($compareEmail !== false) {
         $_SESSION['errors'][] = "Maybe try another email, this already exists!";
         redirect('/register.php');
     }
@@ -25,16 +24,13 @@ if (isset($_POST['username'], $_POST['email'], $_POST['password'])) {
     $statement->bindParam(':username', $username, PDO::PARAM_STR);
     $statement->execute();
     $checkUsername = $statement->fetch(PDO::FETCH_ASSOC);
-    if ($checkUsername) {
+    if ($checkUsername !== false) {
         $_SESSION['errors'][] = "Maybe try another username, this already exists!";
         redirect('/register.php');
     }
-    //add a new user in database:
-    $statement = $database->prepare('INSERT INTO users
-   (username, email, password)
-   VALUES
-   (:username, :email, :password)');
 
+    //add a new user in database:
+    $statement = $database->prepare('INSERT INTO users (username, email, password) VALUES (:username, :email, :password)');
     $statement->bindParam(':username', $username, PDO::PARAM_STR);
     $statement->bindParam(':email', $email, PDO::PARAM_STR);
     $statement->bindParam(':password', $hashed_password, PDO::PARAM_STR);
