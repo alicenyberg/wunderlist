90bc76ab0c7f2ce6a4c3f0b0f083665364b09290
alicenyberg <alice.nyberg@hotmail.com>
2022-01-04 13:30:27 +0100

    updated profile

--- profile.php
+++ profile.php
@@ -11,3 +11,27 @@
     <?php endforeach; ?>
     <?php unset($_SESSION['errors']) ?>
 <?php endif; ?>
+
+<p>Change your profile picture
+<p>
+<form action="app/users/avatar/upload.php" method="post">
+    <label for="avatar">Upload image in JPG/PNG format</label>
+    <input type="file" name="avatar" id="avatar" accept=".png, .jpg, .jpeg">
+    <button type="submit">Upload image</button>
+</form>
+
+<p>Change your email address
+<p>
+<form action="app/users/update/email.php" method="post">
+    <label for="email">new email address:</label>
+    <input type="email" name="email" id="email">
+    <button type="submit">Change email address</button>
+</form>
+
+<p>Change your password
+<p>
+<form action="app/users/update/password" method="post">
+    <label for="password">current password: </label>
+    <input type="password" name="password" id="password">
+    <button type="submit">Upload image</button>
+</form>
