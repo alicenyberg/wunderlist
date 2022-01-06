1b6f23cd6f8a5b3d1bd6959a562b852454b70d7b
alicenyberg <alice.nyberg@hotmail.com>
2022-01-06 11:40:28 +0100

    added image upload

--- app/users/avatar/upload.php
+++ app/users/avatar/upload.php
@@ -3,3 +3,21 @@
 declare(strict_types=1);
 
 require __DIR__ . '../../../autoload.php';
+
+if (isset($_FILES['image'])) {
+    $tmp_name = $_FILES['image']['tmp_name'];
+    $filetype = pathinfo($_FILES['image']['name'], PATHINFO_EXTENSION);
+    $filetype = $_SESSION['user']['id'] . '_avatar.' . $filetype;
+    $user_id = $_SESSION['user']['id'];
+    move_uploaded_file($tmp_name, __DIR__ . '../../../../uploads' . $filename);
+
+    $statement = $database->prepare(
+        'UPDATE users SET image_url = :image_url WHERE id = :user_id'
+    );
+
+    $statement->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+    $statement->bindParam(':image_url', $filename, PDO::PARAM_STR);
+    $statement->execute();
+
+    redirect('/');
+}
