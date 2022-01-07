9bd3d1c203ffb5ac8e7ffa2d3e5268a814b53cb8
alicenyberg <alice.nyberg@hotmail.com>
2022-01-07 11:27:22 +0100

    more in email.php

--- app/users/update/email.php
+++ app/users/update/email.php
@@ -5,11 +5,24 @@ declare(strict_types=1);
 require __DIR__ . '/../../autoload.php';
 
 if (isset($_POST['email'])) {
-    $trimmed_email = trim($_POST['email.']);
+    $trimmed_email = trim($_POST['email']);
     $email = filter_var($trimmed_email, FILTER_SANITIZE_EMAIL);
     $user_id = $_SESSION['user']['id'];
 
     //to check if email already exists:
     $statement = $database->prepare('SELECT email FROM users WHERE email = :email');
-    $statement->bindParam(':email', $email, PDO::PARAM_STR)
+    $statement->bindParam(':email', $email, PDO::PARAM_STR);
+    $statement->execute();
+    $compareEmail = $statement->fetch(PDO::FETCH_ASSOC);
+
+    if ($compareEmail) {
+        $_SESSION['errors'] = "Maybe try another email address, this one already exists.";
+        redirect('/profile.php');
+    }
+
+    //to change the email
+    $statement = $database->prepare('UPDATE users SET email = :email WHERE id = :user_id');
+    $statement->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+    $statement->bindParam(':email', $email, PDO::PARAM_INT);
+    $statement->execute();
 }
