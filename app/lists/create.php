4d008b23ba032ba67ac4b2d258aa6cd2fee85e0b
alicenyberg <alice.nyberg@hotmail.com>
2022-01-07 14:29:38 +0100

    updated create.php

--- app/lists/create.php
+++ app/lists/create.php
@@ -8,4 +8,9 @@ if (isset($_POST['title'])) {
     $user_id = $_SESSION['user']['id'];
     $trim_title = trim($_POST['title']);
     $title = filter_var($trim_title, FILTER_SANITIZE_STRING);
+
+    $statement = $database->prepare('INSERT INTO lists (title, user_id) VALUES (:title, :user_id)');
+    $statement->bindParam(':title', $title, PDO::PARAM_STR);
+    $statement->bindParam(':user_id', $user_id, PDO::PARAM_STR);
+    $statement->execute();
 }
