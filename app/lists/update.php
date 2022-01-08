483312442f4bea41dda077a9e68735fb2a67a981
alicenyberg <alice.nyberg@hotmail.com>
2022-01-08 15:41:39 +0100

    added update list

--- app/lists/update.php
+++ app/lists/update.php
@@ -4,4 +4,16 @@ declare(strict_types=1);
 
 require __DIR__ . '/../autoload.php';
 
+if (isset($_POST['list_id'])) {
+    $list_id = $_POST['list_id'];
+
+    $statement = $database->prepare('DELETE FROM tasks WHERE list_id = :list_id');
+    $statement->bindParam(':list_id', $listId, PDO::PARAM_INT);
+    $statement->execute();
+
+    $statement = $database->prepare('DELETE FROM lists WHERE id = :id');
+    $statement->bindParam(':id', $listId, PDO::PARAM_INT);
+    $statement->execute();
+}
+
 redirect('/');
