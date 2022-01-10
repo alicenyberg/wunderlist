327dac51fb4efbd7f247523c840d5037542b7d26
alicenyberg <alice.nyberg@hotmail.com>
2022-01-10 14:08:08 +0100

    added files and updated delete.php

--- app/task/delete.php
+++ app/task/delete.php
@@ -4,4 +4,14 @@ declare(strict_types=1);
 
 require __DIR__ . '/../autoload.php';
 
-redirect('/');
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $user_id = $_SESSION['user']['id'];
+
+    $statement = $database->prepare('DELETE FROM tasks WHERE user_id = :user_id AND id = :id');
+    $statement->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+    $statement->bindParam(':id', $id, PDO::PARAM_INT);
+    $statement->execute();
+
+    redirect('/');
+}
