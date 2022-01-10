c7538aa3f7dd24dc3130609b51ae8d36f326d0b0
alicenyberg <alice.nyberg@hotmail.com>
2022-01-10 15:16:22 +0100

    added a function for lists

--- app/functions.php
+++ app/functions.php
@@ -8,10 +8,24 @@ function redirect(string $path)
     exit;
 }
 
+
 function logged_in()
 {
     if (isset($_SESSION['user'])) {
         $user = $_SESSION['user'];
         return $user;
     }
+}
+
+function get_all_lists(object $database)
+{
+    $user_id = $_SESSION['user']['id'];
+
+    $statement = $database->prepare("SELECT * from lists WHERE user_id = :user_id");
+    $statement->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+    $statement->execute();
+
+    $lists = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+    return $lists;
 }
--- lists.php
+++ lists.php
@@ -4,11 +4,24 @@ require __DIR__ . '/views/header.php';
 
 ?>
 
+
 <form action="app/lists/create.php" method="POST">
     <label for="title">The name of your list:</label>
     <input type="text" name="title" id="title">
     <button type="submit">Create list</button>
 </form>
 
+<ul>
+    <?php
+    $lists = get_all_lists($database);
+    foreach ($lists as $list) : ?>
+        <li>
+            <?= $list['title'] ?><br>
+            <button>
+                <a href="/app/lists/delete.php?id=<?= $list['id']; ?>">Delete </a>
+            </button>
+        </li>
+    <?php endforeach ?>
+</ul>
 
 <?php require __DIR__ . '/views/footer.php'; ?>
