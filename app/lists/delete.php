385da576f591cb184ea0fa12d6338e227743c1e9
alicenyberg <alice.nyberg@hotmail.com>
2022-01-10 08:31:08 +0100

    added delete

--- app/lists/delete.php
+++ app/lists/delete.php
@@ -4,4 +4,11 @@ declare(strict_types=1);
 
 require __DIR__ . '/../autoload.php';
 
+if (isset($_POST['id'])) {
+    $title = filter_var($_POST['id'], FILTER_SANITIZE_NUMBER_INT);
+    $statement = $database->prepare('DELETE FROM lists WHERE id = :id');
+    $statement->bindParam(':id', $title, PDO::PARAM_INT);
+    $statement->execute();
+}
+
 redirect('/');
