8bb7baf070f21a1d83d57495ffcec8a4b5f28085
alicenyberg <alice.nyberg@hotmail.com>
2022-01-11 17:27:03 +0100

    some update.php

--- app/task/update.php
+++ app/task/update.php
@@ -4,4 +4,23 @@ declare(strict_types=1);
 
 require __DIR__ . '/../autoload.php';
 
+$task_id = $_GET['id'];
+$user_id = $_SESSION['user']['id'];
+
+//deadline
+if (isset($_POST['deadline'])) {
+    $deadline = $_POST['deadline'];
+}
+
+if ($deadline) {
+    $statement = $database->prepare('UPDATE tasks SET deadline_at = :deadline_at WHERE id = :id AND user_id = :user_id');
+
+    $statement->bindParam(':deadline_at', $deadline, PDO::PARAM_STR);
+    $statement->bindParam(':id', $task_id, PDO::PARAM_INT);
+    $statement->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+
+    $statement->execute();
+}
+
+
 redirect('/');
