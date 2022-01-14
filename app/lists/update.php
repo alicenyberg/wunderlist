36f76acb90d64f3f00317bbb63c5e485015dd9cf
alicenyberg <alice.nyberg@hotmail.com>
2022-01-14 14:27:31 +0100

    Update update.php

--- app/lists/update.php
+++ app/lists/update.php
@@ -4,19 +4,19 @@ declare(strict_types=1);
 
 require __DIR__ . '/../autoload.php';
 
-
-$list_id = $_GET['list_id'];
 $user_id = $_SESSION['user']['id'];
+$list_id = $_GET['list_id'];
 
-if (isset($_POST['title'], $_POST['content'], $_POST['deadline'])) {
+
+if (isset($_POST['title'])) {
     $trim_title = trim($_POST['title']);
     $title = filter_var($trim_title, FILTER_SANITIZE_STRING);
 
     $statement = $database->prepare("UPDATE lists SET title = :title WHERE id = :id AND user_id = :user_id");
     $statement->bindParam(':id', $list_id, PDO::PARAM_INT);
-    $statement->bindParam(':title', $title, PDO::PARAM_INT);
+    $statement->bindParam(':title', $title, PDO::PARAM_STR);
     $statement->bindParam(':user_id', $user_id, PDO::PARAM_INT);
     $statement->execute();
 }
 
-redirect('/');
+redirect('/lists.php');
