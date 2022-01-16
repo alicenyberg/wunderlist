5d3fbbd4284855b940fa72825de4c9d05b28b678
alicenyberg <alice.nyberg@hotmail.com>
2022-01-16 15:58:40 +0100

    function for uncompleted and completed tasks

--- app/functions.php
+++ app/functions.php
@@ -57,6 +57,33 @@ function get_tasks_list(PDO $database)
     return $tasks;
 }
 
+function completed_tasks(PDO $database)
+{
+    $user_id = $_SESSION['user']['id'];
+
+    $statement = $database->prepare("SELECT * from tasks WHERE user_id = :user_id AND completed_at IS NOT NULL");
+    $statement->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+    $statement->execute();
+
+    $tasks = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+    return $tasks;
+}
+
+
+function uncompleted_tasks(PDO $database)
+{
+    $user_id = $_SESSION['user']['id'];
+
+    $statement = $database->prepare("SELECT tasks.id, tasks.list_id, tasks.user_id, tasks.content, tasks.title, tasks.deadline_at, tasks.completed_at, lists.title AS list_title FROM tasks INNER JOIN lists ON tasks.list_id = lists.id WHERE tasks.user_id = :user_id AND tasks.completed_at IS NULL");
+    $statement->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+    $statement->execute();
+
+    $tasks = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+    return $tasks;
+}
+
 function get_image(PDO $database)
 {
     $user_id = $_SESSION['user']['id'];
