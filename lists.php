34850d66080fe6de2d5fd40071107a17b103f0bf
alicenyberg <alice.nyberg@hotmail.com>
2022-01-17 13:15:00 +0100

    removed tasks button and moved form

--- lists.php
+++ lists.php
@@ -5,26 +5,27 @@ require __DIR__ . '/views/header.php';
 ?>
 
 <!-- here we can create a list -->
-<ul>
-    <?php
-    $lists = get_all_lists($database);
-    foreach ($lists as $list) : ?>
-        <li>
-            <h4> <?= $list['title'] ?></h4><br>
-            <button>
-                <a href="edit_list.php?id=<?= $list['id']; ?>">Edit your list</a>
-            </button>
-            <button>
-                <a href="edit_task.php?id=<?= $list['id']; ?>">Edit tasks</a>
-            </button>
-        </li>
-    <?php endforeach ?>
-</ul>
+<div>
+    <form action="app/lists/create.php" method="post">
+        <label for="title">The name of your list:</label> <br>
+        <input type="text" name="title" id="title">
+        <button type="submit">Create list</button>
+    </form>
+</div>
 
-<form action="app/lists/create.php" method="post">
-    <label for="title">The name of your list:</label> <br>
-    <input type="text" name="title" id="title">
-    <button type="submit">Create list</button>
-</form>
+<div>
+    <ul>
+        <?php
+        $lists = get_all_lists($database);
+        foreach ($lists as $list) : ?>
+            <li>
+                <h4> <?= $list['title'] ?></h4><br>
+                <button>
+                    <a href="edit_list.php?id=<?= $list['id']; ?>">Edit your list</a>
+                </button>
+            </li>
+        <?php endforeach ?>
+    </ul>
+</div>
 
 <?php require __DIR__ . '/views/footer.php'; ?>
