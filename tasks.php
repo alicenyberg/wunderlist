0383d5a321e5690134342e525fe7e0bc67d9f1c6
alicenyberg <alice.nyberg@hotmail.com>
2022-01-17 13:50:02 +0100

    changed name to tasks

--- tasks.php
+++ tasks.php
@@ -2,7 +2,7 @@
 <?php require __DIR__ . '/views/header.php'; ?>
 
 
-<h2>Uncompleted tasks</h2>
+<h2>Your uncompleted tasks</h2>
 <ul>
     <?php $tasks = uncompleted_tasks($database);
     foreach ($tasks as $task) : ?>
@@ -23,4 +23,18 @@
     <?php endforeach ?>
 </ul>
 
+<h2>Your completed tasks</h2>
+<ul>
+    <?php $tasks = completed_tasks($database);
+    foreach ($tasks as $task) : ?>
+        <?php if ($_SESSION['user']['id']) : ?>
+            <h4> <?= $task['title'] ?> </h4>
+            <li>
+                <p> List: <?= $task['list_title'] ?> <br>
+                    Deadline: <?= $task['deadline_at'] ?> <br>
+                    Description: <?= $task['content'] ?> <br></p>
+            </li>
+        <?php endif ?>
+    <?php endforeach ?>
+</ul>
 <?php require __DIR__ . '/views/footer.php'; ?>
